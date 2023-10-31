735a015eb2bb9db5756b0af69a0bf74485607ac8
Sergio Willames Oliveira Costa <sergiowoc@gmail.com>
2023-10-31 02:58:49 -0300

    setup initial. (#1)
    Co-authored-by: Sergio <tp.sergio.willames@totvspartner.com.br>

---
+++ users/config/application.rb
@@ -0,0 +1,17 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Users
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.generators.system_tests = nil
+    config.api_only = true
+  end
+end
